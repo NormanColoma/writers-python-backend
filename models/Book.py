da3368b6fc023caa52e11305453ce58e3d4a89d0
Norman Coloma García <ua.norman@gmail.com>
2017-10-10 23:22:38 +0200

    Adding property to domain model class

--- models/Book.py
+++ models/Book.py
@@ -1,7 +1,31 @@
 class Book:
-    id: str = ""
-    name: str = ""
-    description: str = ""
+    __id: str = ""
+    __name: str = ""
+    __description: str = ""
 
     def __init__(self):
         pass
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def description(self):
+        return self.__description
+
+    @id.setter
+    def id(self, __id: str):
+        self.__id = __id
+
+    @name.setter
+    def name(self, name: str):
+        self.__name = name
+
+    @description.setter
+    def description(self, desc: str):
+        self.__description = desc
